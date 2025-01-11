f94d7960acf983a2b69b9e9fff0a08ebab45e873
wynprtm <wayanpratama38@gmail.com>
2025-01-12 00:17:35 +0700

    Adding Animation When Expanded List

--- app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +16,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.outlined.ExpandMore
@@ -53,6 +59,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     var isExpanded by remember{mutableStateOf(false)}
+    val animatedSizeDp by animateDpAsState(
+        targetValue =  if(isExpanded) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness =  Spring.StiffnessLow
+        )
+    )
 
     Row (
         modifier = Modifier.padding(8.dp),
@@ -61,7 +74,7 @@ fun Greeting(name: String) {
         Image(
             painter = painterResource(R.drawable.jetpackcompose),
             contentDescription = "Logo Jetpack Compose",
-            modifier = Modifier.size(80.dp)
+            modifier = Modifier.size(animatedSizeDp)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column (modifier = Modifier.weight(1f)){
@@ -109,13 +122,15 @@ fun NewComposeAble(){
 @Composable
 fun GreetingList(names:List<String>){
     if(names.isNotEmpty()){
-        Column {
-            for (name in names){
+        LazyColumn {
+            items(names){name->
                 Greeting(name)
             }
         }
     }else{
-        Text("No People to greet")
+        Box(contentAlignment = Alignment.Center){
+            Text("No People to greet")
+        }
     }
 
 }
