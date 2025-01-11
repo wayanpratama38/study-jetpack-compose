331d04dd9b98ff85ece977e8855cde36f57bb36e
wynprtm <wayanpratama38@gmail.com>
2025-01-11 23:52:29 +0700

    Adding List of Name In Jetpack Compose

--- app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
@@ -12,14 +12,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,7 +44,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Row {
+    Row (
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Image(
             painter = painterResource(R.drawable.jetpackcompose),
             contentDescription = "Logo Jetpack Compose",
@@ -62,10 +69,44 @@ fun Greeting(name: String) {
 
 }
 
-@Preview(showBackground = true)
+private val sampleName = listOf(
+    "Andre",
+    "Desta",
+    "Parto",
+    "Wendy",
+    "Komeng",
+    "Raffi Ahmad",
+    "Andhika Pratama",
+    "Vincent Ryan Rompies"
+)
+
+@Composable
+fun NewComposeAble(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        GreetingList(sampleName)
+    }
+}
+
+@Composable
+fun GreetingList(names:List<String>){
+    if(names.isNotEmpty()){
+        Column {
+            for (name in names){
+                Greeting(name)
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
 fun GreetingPreview() {
     JetpackComposeTestTheme {
-        Greeting("Adit")
+//        Greeting("Adit")
+        NewComposeAble()
     }
 }
